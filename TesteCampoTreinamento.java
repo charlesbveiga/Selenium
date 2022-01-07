112d34baa32b0e5845ae5f35382f8a057e814f29
Charles Batista Veiga <84667490+charlesbveiga@users.noreply.github.com>
2022-01-06 23:40:05 -0300

    Add files via upload

--- TesteCampoTreinamento.java
+++ TesteCampoTreinamento.java
@@ -8,10 +8,12 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.By.ById;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+
 
 public class TesteCampoTreinamento {
 	
@@ -111,5 +113,16 @@ public class TesteCampoTreinamento {
 		Assert.assertEquals("Campo de Treinamento",dsl.obterTexto(By.tagName("h3"))); 
 				
 		Assert.assertEquals("Cuidado onde clica, muitas armadilhas...",dsl.obterTexto(By.className("facilAchar"))); 
+	}
+	
+	@Test
+	public void testJavascript() {
+		JavascriptExecutor js = (JavascriptExecutor) driver; 	
+		//js.executeScript("alert('Testando JS via Selenium')");
+		js.executeScript("document.getElementById('elementosForm:nome').value = 'Escrito via JS'");
+		js.executeScript("document.getElementById('elementosForm:sobrenome').type = 'radio'");
+		
+		WebElement element = driver.findElement(By.id("elementosForm:nome"));
+		js.executeScript("arguments[0].style.border = arguments[1]", element, "solid 4px red");
 	}
 }
